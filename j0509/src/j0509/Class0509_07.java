731ece4da465776c217b8649ae6372e5d54dd748
Administrator <Administrator@DESKTOP-7G4AVL4>
2023-06-01 20:11:31 +0900

    may 9

--- j0509/src/j0509/Class0509_07.java
+++ j0509/src/j0509/Class0509_07.java
@@ -15,7 +15,7 @@ public class Class0509_07 {
 		System.out.println("1개의 정수를 입력하세요");
 		input = scan.nextInt();
 				
-		//3. 출력
+		//3. 출력 
 		String answer = (input%2==0)?"짝수입니다.":"홀수입니다.";
 		System.out.println(answer);
 
